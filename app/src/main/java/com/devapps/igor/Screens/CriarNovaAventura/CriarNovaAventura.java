c0482eef22b93e949c0cd944e25908804b8f5c73
Hercules Nunes <herculessnunes@gmail.com>
2017-09-09 21:13:25 -0300

    Activity and layout created for CriarAventura screen. The drawable resources needed were added.

---
+++ app/src/main/java/com/devapps/igor/Screens/CriarNovaAventura/CriarNovaAventura.java
@@ -0,0 +1,15 @@
+package com.devapps.igor.Screens.CriarNovaAventura;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.devapps.igor.R;
+
+public class CriarNovaAventura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_criar_nova_aventura);
+    }
+}
